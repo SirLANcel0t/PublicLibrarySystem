c2a2e2428152d61950fefd1a9e66a81c710af5e1
BR1005978 <1005978@hr.nl>
2021-04-21 15:48:20 +0200

    basis gemaakt van classes en hun methods met omschrijving van wat er moet komen

--- PLS.py
+++ PLS.py
@@ -1,1 +1,96 @@
 print("Hello World!")
+
+
+# het maken van PublicLibrary, de class
+class PublicLibrary:
+    """
+    De PublicLibrary class heeft onder andere de lijst van klanten, de functionaliteit om alle informatie van 
+    boeken en klanten te backuppen en weer in te laden. Klanten kunnen ingeladen worden vanuit een CSV bestand,
+    en klanten moeten hoogstwaarschijnlijk ook opgeslagen worden in een CSV bestand. 
+
+    Is dit een abstract class? worden hier objecten van gemaakt? of blijft het alleen bij deze class? 
+    """
+    def __init__(self, listofcustomers):
+        self.listofcustomers = listofcustomers
+    
+    def backupSystem():
+        """
+        een systeembackup slaat alle informatie over boeken en gebruikers op. 
+        De vraag is alleen, moet alles in een .JSON bestand? Of gaan de boeken in een JSON bestand en de klanten
+        in een .CSV bestand? 
+        """
+        pass
+
+    def loadSystemBackup():
+        """
+        Het inladen van een gemaakte backup. Waarschijnlijk bestaat dit uit een .JSON bestand met boeken, en een .CSV bestand
+        met klanten.
+        """ 
+        pass
+
+    def loadCustomers():
+        """
+        Het inladen van een lijst met klanten middels een .CSV bestand.
+        """
+        pass
+
+    def exportCustomers():
+        """
+        Klanten worden ingeladen middels het .CSV bestand wat we meegekregen hebben vanuit de opdracht, dus het is aannemelijk
+        om er vanuit te gaan dat het exporteren van klanten ook in een .CSV bestand moet.
+        Is deze functie overbodig? Is dit onderdeel van de functie backupSystem? 
+        """
+        pass
+
+
+
+class Person:
+    """
+    dit is de class Person, een superclass van librarian  en Customer. person is een 
+    abstract class want van Person bestaan geen objecten. 
+
+    """ 
+    mayAddBooks = False
+
+class Librarian(Person):
+    """
+    librarian is een subclass van Person. de librarian is de beheerder van het systeem en kan het systeem backuppen,
+    een backup inladen, bookitems toevoegen en verwijderen, klanten toevoegen (zowel individueel als via een CSV bestand)
+    boeken uitlenen voor klanten, en via een JSON bestand allemaal boeken inladen. 
+    van librarian kunnen wel objecten gemaakt worden. je zou immers meerdere librarians kunnen hebben
+    """
+
+    mayAddBooks = True
+
+class Subscriber(Person):
+    """
+    Subscriber is een klant. Objecten van deze class kunnen boeken zoeken, boekeninformatie inzien, boeken lenen voor een
+    bepaalde tijd. 
+    """
+
+    # nu worden deze variableen nog in Customer class gemaakt, maar misschien kunen die al in
+    # person class gedefineerd worden? 
+
+    def __init__(self, userNumber, gender, nameSet, givenName, surName, streetAdress, zipCode, city, emailAdress, userName, password, telephoneNumber):
+        self.userNumber = userNumber
+        self.gender = gender
+        self.nameSet = nameSet
+        self.givenName = givenName
+        self.surName = surName
+        self.streetAdress = streetAdress
+        self.zipCode = zipCode
+        self.city = city
+        self.emailAdress = emailAdress
+        self.userName = userName
+        self.password = password
+        self.telephoneNumber = telephoneNumber
+    
+    # het is misschien niet heel logisch om login bij Customer te hebben
+    # ik bedoel de customer krijg je als je inlogt, toch? 
+    def login():
+        pass
+
+nextInLine = # json bestand uitlezen en laatste cijfer zoeken
+
+hans = Customer(nextInLine,"male", "Dutch", "Hans", "de Boer", "Lange Lindelaan 14", "3011BB", "Rotterdam", "hansjepansje@gmail.com", "masterhans123", "0619283755")
+
